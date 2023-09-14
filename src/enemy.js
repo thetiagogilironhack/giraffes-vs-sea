cdb6a6d03a40266d82c6ba0877a3e966f0fa753b
Tiago Gil <talmeidagil@hotmail.com>
2023-09-14 18:37:28 +0100

    added src files

---
+++ src/enemy.js
@@ -0,0 +1,7 @@
+class Enemy {
+    constructor(strength, velocity, spawn){
+        this.strength = strength;
+        this.velocity = velocity;
+        this.spawn = spawn;
+    }
+}
---
+++ src/player.js
@@ -0,0 +1,5 @@
+class Player {
+  constructor(health) {
+    this.health = 100;
+  }
+}
